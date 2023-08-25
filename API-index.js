997734b6f7bb359541d8fbd7f63bbe49ce82db06
Andreas <andreaskodehode4@gmail.com>
2023-08-25 10:03:22 +0200

    image folder

--- API-index.js
+++ API-index.js
@@ -10,6 +10,7 @@ window.onload = async function() {
         pokemon.id = i
         pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase()
         pokemon.classList.add("pokemon-name")
+        pokemon.addEventListener("click", updatePokemon)
         document.getElementById("pokemon-list").append(pokemon)
     }
     console.log(pokedex)
@@ -32,4 +33,25 @@ async function getPokemon(num) {
     pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"]
 
     pokedex [num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc}
+}
+
+function updatePokemon() {
+    document.getElementById("pokemon-img").src = pokedex[this.id]["img"]
+    //clear previous type
+    let typesDiv = document.getElementById("pokemon-types")
+    while (typesDiv.firstChild) {
+        typesDiv.firstChild.remove()
+    }
+    //update type
+    let types = pokedex[this.id]["types"]
+    for(let i = 0; i < types.length; i++) {
+        let type = document.createElement("span")
+        type.innerText = types[i]["type"]["name"].toUpperCase()
+        type.classList.add("type-box")
+        type.classList.add(types[i]["type"]["name"])
+        typesDiv.append(type)
+
+    }
+
+    document.getElementById("pokemon-desc").innerText = pokedex[this.id]["desc"]
 }
